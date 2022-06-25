c03ba8ccb2504f471a1e5c494f562d253076fc69
nukky0113 <56378055+nukky0113@users.noreply.github.com>
2022-06-25 23:08:49 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import "./styles.css";
+
+export default function App() {
+  const onClickUsers = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  const onClickUsers1 = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users/3")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div className="App">
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUsers1}>id=1のuser</button>
+    </div>
+  );
+}
